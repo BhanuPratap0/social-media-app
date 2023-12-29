f047a3cd95e77bc9c8bdd2832fe339804ff806ef
BhanuPratap0 <119663548+BhanuPratap0@users.noreply.github.com>
2023-12-29 18:52:23 +0530

    new

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,61 +1,77 @@
 import { Chat, Notifications, Person, Search } from "@mui/icons-material"
 import "./topbar.css"
 import { Link } from "react-router-dom"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
+import axios from "axios"
 
 export default function Topbar() {
 
   const { user } = useContext(AuthContext)
-
-  const searchUser = async() => {}
+  const [searchResult, setSearchResult] = useState([]);
+  const searchUser = async (query) => {
+    if (!query) {
+      setSearchResult([])
+      return;
+    } else {
+      try {
+        const {data}  = await axios.get(`http://localhost:8800/api/user/searchUsers?search=${query}`, { userId: user._id });
+        console.log(data);
+        setSearchResult(data);
+      } catch (error) {
+        console.log("no user to search")
+      }
+    }
+  }
 
   return (
     <>
-    <div className="topbarContainer">
-      <div className="topbarLeft">
-        <Link to="/" style={{ textDecoration: "none" }} >
-          <span className="logo">SociSync</span>
-          <span className="sublogo">BS</span>
-        </Link>
-      </div>
-      <div className="topbarCenter">
-        <div className="searchbar">
-          <Search className="searchIcon" />
-          <input onChange={searchUser} placeholder="Search for friend, post or video" className="searchInput" />
+      <div className="topbarContainer">
+        <div className="topbarLeft">
+          <Link to="/" style={{ textDecoration: "none" }} >
+            <span className="logo">SociSync</span>
+            <span className="sublogo">BS</span>
+          </Link>
         </div>
-          
-      </div>
-      <div className="topbarRight">
-        <div className="topbarLinks">
-          <span className="topbarLink">Homepage</span>
-          <span className="topbarLink">Timeline</span>
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <Search className="searchIcon" />
+            <input onChange={(e) => searchUser(e.target.value)} placeholder="Search for friend, post or video" className="searchInput" />
+          </div>
+
         </div>
-        <div className="topbarIcons">
-          <div className="topbarIconItem">
-            <Person />
-            <span className="topbarIconBadge">1</span>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Homepage</span>
+            <span className="topbarLink">Timeline</span>
           </div>
-          <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">1</span>
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <Person />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Chat />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Notifications />
+              <span className="topbarIconBadge">1</span>
+            </div>
           </div>
-          <div className="topbarIconItem">
-            <Notifications />
-            <span className="topbarIconBadge">1</span>
-          </div>
+          <Link to={`/profile/${user.username}`} >
+            <img src={user.profilePicture} alt="" className="topbarImg" />
+          </Link>
         </div>
-        <Link to={`/profile/${user.username}`} >
-          <img src={user.profilePicture} alt="" className="topbarImg" />
-        </Link>
+
       </div>
-      
-    </div>
-    <div className="serachUserContainer" >
-      <div className="searchUserItem">Bhanu</div>
-      <div className="searchUserItem">Bhanu</div>
-      <div className="searchUserItem">Bhanu</div>
-    </div>
+      {searchResult!==null && searchResult.map((result) => (
+        <>
+          <div className="serachUserContainer" >
+            <div className="searchUserItem">{result.username}</div>
+          </div >
+        </>
+      ))}
     </>
   )
 }
