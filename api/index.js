da8a8ef82c5b38e524223728e0dbe15f208754a3
BhanuPratap0 <119663548+BhanuPratap0@users.noreply.github.com>
2024-01-16 12:42:32 +0530

    secure

--- api/index.js
+++ api/index.js
@@ -40,8 +40,8 @@ app.use(session({
     saveUninitialized: false, //dont create a session until something is stored
     cookie: {
         maxAge: 24 * 60 * 60 * 1000, // 7 days
-        //Enable when deployment OR when not using localhost, this wont work without https
-        // sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
+        secure: true,//Enable when deployment OR when not using localhost, this wont work without https
+        sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
     },
 }));
 
